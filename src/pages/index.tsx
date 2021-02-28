7d5d3199bdbdce61485703896e053b1e446f01eb
Felipe Leal <felipewanderson2020@gmail.com>
2021-02-28 18:29:31 -0300

    pagina inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,61 +1,46 @@
-import { GetServerSideProps } from 'next';
-import Head from 'next/head';
+import Link from 'next/link'
+import { ChangeEvent, useCallback, useState } from 'react'
+import styles from '../styles/pages/Login.module.css'
+export default function Home() {
+  const [isFulled, setIsFulled] = useState(false);
 
+  const handleChangeText = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    const {value} = event.target;
+    if (!value) {
+      setIsFulled(false);
+      return;
+    }
 
-import { ExperienceBar } from '../components/ExprerienceBar';
-import { CompletedChallenger } from '../components/CompletedChallenger';
-import { Countdown } from '../components/Countdown';
-import { ChallengeBox } from '../components/ChallengeBox';
-import { Profile } from '../components/Profile';
-
-import styles from '../styles/pages/index.module.css';
-import { CountdownProvider } from '../hooks/Countdown';
-import { ChallengesProvider } from '../hooks/Challenge';
-
-
-interface HomeProps {
-  level: number,
-  currentExperience: number,
-  challengesCompleted: number,
-}
-export default function Home(props: HomeProps) {
+    setIsFulled(true);
+  }, [isFulled]);
   return (
-    <ChallengesProvider
-      level={props.level}
-      currentExperience={props.currentExperience}
-      challengesCompleted={props.challengesCompleted}
-    >
+    <div className={styles.background}>
       <div className={styles.container}>
-        <Head>
-          <title>Início | move.it
-          </title>
-        </Head>
-        <ExperienceBar />
-        <CountdownProvider>
-          <section>
-            <div>
-              <Profile />
-              <CompletedChallenger />
-              <Countdown />
+        <div className={styles.logo}>
+          <img src="/logo-transparent.svg" alt="Move.it - Logo"/>
+        </div>
+        <div className={styles.login}>
+          <img src="/logo-full-white.svg" alt="Move.it - Logo"/>
+          <div className={styles.loginContent}>
+            <strong>Bem-vindo</strong>
+            <p>
+              <img src="/github.svg" alt="Icone Github"/>
+              Faça login com seu github para começar
+            </p>
+            <div className={styles.inputContainer}>
+              <input type="text" onChange={e => handleChangeText(e)}/>
+              <Link href="/challenges">
+                <a className={isFulled ? styles.fulled: undefined}>
+                  <img src="/arrow-left.svg" alt="Seta para esquerda"/>
+                </a>
+              </Link>
             </div>
-            <div>
-              <ChallengeBox />
-            </div>
-          </section>
-        </CountdownProvider>
+          </div>
+        </div>
       </div>
-    </ChallengesProvider>
+    </div>
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
-  return {
-    props: {
-      level: Number(level),
-      currentExperience: Number(currentExperience),
-      challengesCompleted: Number(challengesCompleted),
-    }
-  }
-}
+
 
